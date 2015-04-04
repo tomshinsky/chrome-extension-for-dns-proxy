bf53aabcd7812efbf6cfc275b4b1f30ff253e258
tomshinsky <tomshinsky@gmail.com>
2015-04-04 14:53:00 +0300

    asd
    asd

--- proxy.js
+++ proxy.js
@@ -2,7 +2,7 @@
 	        mode: "pac_script",
 	        pacScript: {
 	          data: "function FindProxyForURL(url, host) {\n" +
-	                "  if (host == 'www.pandora.com')\n" +
+	                "  if (host == 'pandora.com')\n" +
 	                "    return 'PROXY 107.170.15.247:80';\n" +
 	                "  return 'DIRECT';\n" +
 	                "}"
